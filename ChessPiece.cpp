348f9e63e8b60e8e8af1fa903852f9be1ceba320
Dan <dsohn45@gmail.com>
2019-09-27 00:49:38 -0400

    trajectory for bishop rook king

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -14,9 +14,29 @@ int Pawn::valid_move_shape(Position start, Position end, std::vector<Position>& 
 int Rook::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
 	
 	if (start.x == end.x && start.y != end.y) {
+		if (start.y > end.y) {
+			move = -1;
+		} else {
+			move = 1;
+		}
+		curr = start.y;
+		while (curr != end.y) {
+			trajectory.push_back(Position(start.x, curr));
+		}
 		return SUCCESS;
 	}
+
 	if (start.x != end.x && start.y == end.y) {
+		if (start.x > end.x) {
+			move = -1;
+		} else {
+			move = 1;
+		}
+		curr = start.x;
+		while (curr != end.x) {
+			curr += move;
+			trajectory.push_back(Position(start.x, curr));	
+		}
 		return SUCCESS;
 	}
 	return MOVE_ERROR_ILLEGAL;
@@ -37,6 +57,31 @@ int Bishop::valid_move_shape(Position start, Position end, std::vector<Position>
 	diffy = abs(start.y - end.y);
 
 	if (diffx == diffy) {
+
+		int move_x = 1;
+		int move_y = 1; 
+
+		if (end.x < start.x) {
+			move_x = -1
+		} else if (end.x == start.x) {
+			move_x = 0;
+		}
+
+		if (end.y < start.y) {
+			move_y = -1;
+		} else if (end.y == start.y) {
+			move_y = 0;
+		}
+
+		int curr_x = start.x + move_x;
+		int curr_y = start.y + move_y;
+
+		while(curr_x != end.x && curr_y != end.y) {
+			trajectory.push_back(Position(curr_x, curr_y));
+			curr_x += move_x;
+			curr_y += move_y;
+		} 
+
 		return SUCCESS;
 	}
 	return MOVE_ERROR_ILLEGAL;
@@ -82,6 +127,7 @@ int King::valid_move_shape(Position start, Position end, std::vector<Position>& 
 
 	if ((diffx == 1 || diffx == 0) && (diffy == 1 || diffy == 0)){
 		if (!(diffx == 0 && diffy == 0)) {
+			trajectory.push_back(end)
 			return SUCCESS;
 		}
 	}
