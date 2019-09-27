ad32be08719ce5b783362fe3c63d63e7f223ea2b
Tom Wan <tomwan2008@gmail.com>
2019-09-26 23:58:25 -0400

    Added Knight and Queen

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -1,4 +1,11 @@
 #include "ChessPiece.h"
+int abs(int x) {
+	if (x < 0) {
+		return -x;
+	} else {
+		return x;
+	}
+}
 
 int Pawn::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
 	return success
@@ -16,7 +23,13 @@ int Rook::valid_move_shape(Position start, Position end, std::vector<Position>& 
 }
 
 int Knight::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
-	return success
+	if (abs(end.x - start.x) == 2 && abs(end.y - start.y) == 1 || 
+		abs(end.x - start.x) == 1 && abs(end.y - start.y) == 2) {
+		trajectory.push_back(end);
+		return 1;
+	} else {
+		return 0
+	}
 }
 
 int Bishop::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
@@ -30,7 +43,37 @@ int Bishop::valid_move_shape(Position start, Position end, std::vector<Position>
 }
 
 int Queen::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
-	return success
+	int move_x = 1;
+	int move_y = 1; 
+
+	if (end.x < start.x) {
+		move_x = -1
+	} else if (end.x == start.x) {
+		move_x = 0;
+	}
+
+	if (end.y < start.y) {
+		move_y = -1;
+	} else if (end.y == start.y) {
+		move_y = 0;
+	}
+
+	if (abs(end.x - start.x) > 0 && abs(end.y - start.y) == 0 || 
+		abs(end.y - start.y) > 0 && abs(end.x - start.x) == 0 || 
+		abs(end.y - start.y) == abs(end.x - start.x)) {
+			
+		int curr_x = start.x + move_x;
+		int curr_y = start.y + move_y;
+
+		while(curr_x != end.x && curr_y != end.y) {
+			trajectory.push_back(Position(curr_x, curr_y));
+			curr_x += move_x;
+			curr_y += move_y;
+		} 
+		return 1;
+	} else {
+		return -1;
+	}
 }
 
 int King::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
@@ -39,9 +82,8 @@ int King::valid_move_shape(Position start, Position end, std::vector<Position>& 
 
 	if ((diffx == 1 || diffx == 0) && (diffy == 1 || diffy == 0)){
 		if (!(diffx == 0 && diffy == 0)) {
-			return SUCESS;
+			return SUCCESS;
 		}
 	}
 	return MOVE_ERROR_ILLEGAL;
 }
-
