dcdf5f662a74b2cbfe22f6af0899682412e9e445
Dan <dsohn45@gmail.com>
2019-09-26 23:54:50 -0400

    Rook bishop and king move shape

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -5,7 +5,14 @@ int Pawn::valid_move_shape(Position start, Position end, std::vector<Position>& 
 }
 
 int Rook::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
-	return success
+	
+	if (start.x == end.x && start.y != end.y) {
+		return SUCCESS;
+	}
+	if (start.x != end.x && start.y == end.y) {
+		return SUCCESS;
+	}
+	return MOVE_ERROR_ILLEGAL;
 }
 
 int Knight::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
@@ -13,7 +20,13 @@ int Knight::valid_move_shape(Position start, Position end, std::vector<Position>
 }
 
 int Bishop::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
-	return success
+	diffx = abs(start.x - end.x);
+	diffy = abs(start.y - end.y);
+
+	if (diffx == diffy) {
+		return SUCCESS;
+	}
+	return MOVE_ERROR_ILLEGAL;
 }
 
 int Queen::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
@@ -21,6 +34,15 @@ int Queen::valid_move_shape(Position start, Position end, std::vector<Position>&
 }
 
 int King::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
-	return success
+	diffx = abs(start.x - end.x)
+	diffy = abs(start.y - end.y)
+
+	if (diffx == 1 && diffy == 0) {
+		return SUCCESS;
+	}
+	if (diffx == 0 && diffy == 1) {
+		return SUCESS;
+	}
+	return MOVE_ERROR_ILLEGAL;
 }
 
