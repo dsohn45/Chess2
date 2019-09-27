9336af6824007925cbef459f43440e1b7dce6ba6
Dan <dsohn45@gmail.com>
2019-09-26 23:36:28 -0400

    Init chesspiece.cpp

---
+++ ChessPiece.cpp
@@ -0,0 +1,26 @@
+#include "ChessPiece.h"
+
+int Pawn::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
+	return success
+}
+
+int Rook::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
+	return success
+}
+
+int Knight::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
+	return success
+}
+
+int Bishop::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
+	return success
+}
+
+int Queen::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
+	return success
+}
+
+int King::valid_move_shape(Position start, Position end, std::vector<Position>& trajectory) const {
+	return success
+}
+
